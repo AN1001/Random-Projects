0ff1b06d77b2f494609cd862d3ace894be2dce5f
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2024-09-11 17:08:16 +0100

    Update matrix_handler.py

--- matrix_handler.py
+++ matrix_handler.py
@@ -16,20 +16,18 @@ class matrix():
                 return col
 
         def __mul__(self, other):
-                matrix = [[0 for x in range(len(other.data[0]))] for x in range(len(self.data))]
+                output_matrix = [[0 for x in range(len(other.data[0]))] for x in range(len(self.data))]
                 for i,row in enumerate(self.data):
                         for j in range(len(other.data[0])):
-                                matrix[i][j] = self.dot_product(other.get_col(j), row)
+                                output_matrix[i][j] = self.dot_product(other.get_col(j), row)
 
-                return matrix
+                return matrix(output_matrix)
 
 
         def __str__(self):
                 return "\n".join([" ".join([str(el) for el in row]) for row in self.data])
 
-
 #Examples
 mymat = matrix([[1,2,3],[4,5,6]])
 mymat2 = matrix([[7,8],[9,10],[11,12]])
-newmat = (mymat * mymat2)
-print(newmat.__str__())
+print(mymat * mymat2)
